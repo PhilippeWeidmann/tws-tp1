4d7612a42ac35c9753901f414c411e00b59ee69c
MagicMustache <milo.gianinazzi@gmail.com>
2020-10-19 15:30:31 +0200

    osm (not working for now)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const parser = require('fast-xml-parser');
 const fs = require('fs');
 const {v4: uuid} = require('uuid');
+const overpass = require("query-overpass")
 
 const schemeHeader = "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n" +
     "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.\n" +
@@ -21,7 +22,7 @@ function parseGPX(gpxFile) {
             lat: Number(point.attrsMap["@_lat"]),
             lon: Number(point.attrsMap["@_lon"]),
             ele: point.child.ele[0].val
-        };
+    };
         parsedPoints.push(parsedPoint);
     });
 
@@ -34,8 +35,31 @@ function generateGraphDBPoint(point) {
     schemeString += pointId + ' :lat ' + point.lat + ' .\n';
     schemeString += pointId + ' :lon ' + point.lon + ' .\n';
     schemeString += pointId + ' :ele ' + point.ele + ' .\n';
-
+    //faut mettre que 3 decimal sinon ca marche pas avec osm
+    let lat1 = point.lat.toFixed(3);
+    let lon1 = point.lon.toFixed(3);
+    //test en ajoutnt +0.02
+    let lat2 = +lat1+ +0.02;
+    let lon2 = +lon1+ +0.02;
+    lat2 = lat2.toFixed(3)
+    lon2 = lon2.toFixed(3)
+    //osm(lat1,lat2,lon1,lon2)
     return {id: pointId, value: schemeString};
+}
+
+//marche pas je sais pas pourquoi (que des timeouts)
+function osm(lat1,lat2,lon1,lon2){
+    let query = "[timeout:900];(node("+lat1+","+lon1+","+lat2+","+lon2+"); <; ); out meta;";
+    console.log(query)
+    overpass(query,(error,data)=>{
+        if(error){
+            console.log(error)
+            //setTimeout(()=>{osm(lat1,lat2,lon1,lon2)},200)
+        }
+        else{
+            console.log(data.features[0].properties)
+        }
+    })
 }
 
 function generateGraphDBScheme(gpx) {
@@ -70,4 +94,14 @@ fs.readFile('gpx/4sDDFdd4cjA.gpx', 'utf8', function (err, data) {
         if (err) throw err;
         console.log('Saved!');
     });
+
+    //celle la marche
+    /*overpass("[timeout:900];(node(45.808,6.978,45.828,6.998); <; ); out meta;",(error,result)=>{
+        if(error){
+            console.log(error)
+        }
+        else{
+            console.log(result.features[0].properties)
+        }
+    })*/
 });
